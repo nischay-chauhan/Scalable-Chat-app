704c42bf81bf484604a85b2ffd51efa76f1f55ad
nischay-chauhan <masternischay@outlook.com>
2025-10-19 14:03:03 +0530

    User functions

---
+++ server/src/socket/user.ts
@@ -0,0 +1,96 @@
+import db , {generateId} from "../db";
+import { User } from "./type";
+
+export const addUser = async(username : string , room : string) => {
+    const id = generateId();
+
+    try{
+        await new Promise((resolve , reject) => {
+            db.run(
+                "INSERT INTO users (id , username , room) VALUES (?, ?, ?)" , 
+                [id, username, room] ,
+                (error) => {
+                    if(error){
+                        reject(error)
+                    }else{
+                        resolve(true)
+                    }
+                }
+            )
+        })
+        return {success : true , user : {id , username , room}}
+
+    }catch(error : any){
+       return {success : false , error : error.toString()}
+    }
+}
+
+
+export const removeUser = async(id : string) => {
+    try{
+        await new Promise((resolve , reject) => {
+            db.run(
+                "DELETE FROM users WHERE id = ?" , 
+                [id] ,
+                (error) => {
+                    if(error){
+                        reject(error)
+                    }else{
+                        resolve(true)
+                    }
+                }
+            )
+        })
+        return {success : true , message : "User removed successfully"}
+
+    }catch(error : any){
+       return {success : false , error : error.toString()}
+    }
+}
+
+
+export const getUserByUsername = async(username : string) => {
+    try{
+        const user = await new Promise<any>((resolve , reject) => {
+            db.all(
+                "SELECT * FROM users WHERE username = ?" , 
+                [username] ,
+                (error , rows) => {
+                    if(error){
+                        reject(error)
+                    }else{
+                        resolve(rows && rows[0] ? rows[0] : null)
+                    }
+                }
+            )
+        })
+        return {success : true , user}
+
+    }catch(error : any){
+       return {success : false , error : error.toString()}
+    }
+}
+
+
+export const getUsersInRoom = async (room:string) => {
+    try{
+        const users = await new Promise<any>((resolve , reject) => {
+            db.all(
+                "SELECT * FROM users WHERE room = ?" , 
+                [room] ,
+                (error , rows) => {
+                    if(error){
+                        reject(error)
+                    }else{
+                        resolve(rows)
+                    }
+                }
+            )
+        })
+        return {success : true , users}
+
+    }catch(error : any){
+       return {success : false , error : error.toString()}
+    }
+}
+
